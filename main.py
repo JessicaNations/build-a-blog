81ae00bd24d35b811e16c720b6547826227f900a
Jessica Nations <jessica.nations@gmail.com>
2018-04-14 14:11:31 -0500

    final tweak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, redirect, render_template, session
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -8,14 +9,16 @@ app.config["SQLALCHEMY_ECHO"] = True
 db = SQLAlchemy(app)
 
 #create class for blog posts (5000 char limit on content set to approx 1000 words)
-class BlogPost(db.Model):
+class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200))
     content = db.Column(db.String(6000))
+   
 
     def __init__(self, title, content):
         self.title = title
         self.content = content
+
 
 
 #APP ROUTES / HANDLERS
@@ -34,18 +37,18 @@ def index():
         if not new_title or not new_content:
             return render_template("newpost.html",title=new_title, content=new_content, error_message="DON'T BE SO STUPID")
 
-        new_post = BlogPost(new_title, new_content)
+        new_post = Blog(new_title, new_content)
         db.session.add(new_post)
         db.session.commit()
-        return redirect("/blog?post_id="+str(new_post.id))
+        return redirect("/blog?id="+str(new_post.id))
     
-    view_post_id = request.args.get("post_id")
+    view_post_id = request.args.get("id")
     if view_post_id:
-        view_post = BlogPost.query.get(int(view_post_id))
+        view_post = Blog.query.get(int(view_post_id))
     else:
         view_post = ""
     
-    posts = BlogPost.query.order_by(BlogPost.id.desc()).all()
+    posts = Blog.query.order_by(Blog.id.desc()).all()
 
     return render_template("blog.html", posts=posts, view_post=view_post)
 
